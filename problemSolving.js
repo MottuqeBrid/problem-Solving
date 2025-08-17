f632827c9abb3bdbdf2c44120fe732d9b8e461f8
Mottuqe Brid <mottuqebrid@gmail.com>
2025-08-18 00:49:07 +0600

    Add countVowels function to count vowels in a string

--- problemSolving.js
+++ problemSolving.js
@@ -4,4 +4,18 @@ function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
-console.log(reverseString("hello")); // Output: "olleh"
+console.log(reverseString("hello"));
+
+// Problem 2: Count Vowels in a String
+function countVowels(str) {
+  const vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+  let count = 0;
+  const arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (vowels.includes(arr[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowels("programming"));
