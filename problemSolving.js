922761d90cdfeb17350fbbd29c8d32485ed1ca50
Mottuqe Brid <mottuqebrid@gmail.com>
2025-08-18 01:18:36 +0600

    Add factorial function to calculate the factorial of a number

--- problemSolving.js
+++ problemSolving.js
@@ -84,4 +84,19 @@ function capitalizeFirstLetter(str) {
 }
 console.log(capitalizeFirstLetter("hello world sdfvs gdf"));
 
+// Problem 9: Find the Factorial of a Number
+function factorial(n) {
+  let result = 1;
+  if (n < 0) {
+    return "Factorial is not defined for negative numbers";
+  }
+  if (n === 0) {
+    return 1;
+  }
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
 
+console.log(factorial(2));
